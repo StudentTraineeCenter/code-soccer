f32ccdc52c1aee3d771b2efc619da18b5512dd50
Jakub Wodecki <wodecki@atlasgroup.cz>
2022-11-21 17:44:06 +0100

    Added javadoc comments to methods.

--- src/main/java/stc/soccer/GameEvaluation.java
+++ src/main/java/stc/soccer/GameEvaluation.java
@@ -13,6 +13,11 @@ public class GameEvaluation {
         this.turn = true;
     }
 
+    /**
+     * Method used to start the game.
+     *
+     * @param in for using already pre-existing Scanner.
+     */
     public void play(Scanner in) {
         while (true) {
             turnOutput(turn);
@@ -33,6 +38,11 @@ public class GameEvaluation {
         }
     }
 
+    /**
+     * Method for informing players about their turns.
+     *
+     * @param turn whose purpose is to identify which players turn it is.
+     */
     private void turnOutput(boolean turn) {
         if (turn) {
             System.out.println("Players " + game.getPlayer1() + " turn:");
@@ -43,6 +53,14 @@ public class GameEvaluation {
         System.out.println("Current location of ball is " + game.getBallPosition().toString() + ".");
     }
 
+    /**
+     * Method for validation of a new move.
+     * <p>
+     * This method utilizes other methods to decide, whether a desired move is valid.
+     *
+     * @param dest whose generated move should be validated.
+     * @return true if desired move is valid, otherwise false.
+     */
     private boolean isMoveValid(FieldPoint dest) {
         return (isPointInsideBounds(dest) && isMoveMade(dest) &&
                 isPointCloseEnough(dest) && !isMoveInHistory(dest) &&
@@ -50,11 +68,23 @@ public class GameEvaluation {
                 isPointInsideGoals(dest);
     }
 
+    /**
+     * Method that checks whether a point is inside of bounds.
+     *
+     * @param point whose location should be checked.
+     * @return true if point is inside of bounds, otherwise false.
+     */
     private boolean isPointInsideBounds(FieldPoint point) {
         return ((point.row() >= 0) && (point.row() < game.getRows())) &&
                 ((point.column() >= 0) && (point.column() < game.getColumns()));
     }
 
+    /**
+     * Method for checking whether a point is close enough to the ball.
+     *
+     * @param point whose location should be checked.
+     * @return true if point is close enough, otherwise false.
+     */
     private boolean isPointCloseEnough(FieldPoint point) {
         final FieldPoint position = game.getBallPosition();
 
@@ -62,20 +92,41 @@ public class GameEvaluation {
                 ((position.column() - point.column()) > -2 && (position.column() - point.column()) < 2);
     }
 
+    /**
+     * Method for checking whether a move was made.
+     *
+     * @param point whose relative position to the ball should be checked.
+     * @return true if a move was made, otherwise false.
+     */
     private boolean isMoveMade(FieldPoint point) {
         return (game.getBallPosition().row() != point.row() || game.getBallPosition().column() != point.column());
     }
 
+    /**
+     * Method for checking goals.
+     *
+     * @param point whose location should be checked.
+     * @return true if point is located inside a goal, otherwise false.
+     */
     private boolean isPointInsideGoals(FieldPoint point) {
         return isPointCloseEnough(point) &&
                 (Math.abs(point.column() - game.getColumns() / 2) < 2) &&
                 (point.row() == -1 || point.row() == game.getRows());
     }
 
-    private boolean isMoveInHistory(FieldPoint dest) {
-
-        if (game.getMoveHistory().containsKey(dest)) {
-            final List<FieldPoint> pointHist = game.getMoveHistory().get(dest);
+    /**
+     * Method for checking move history.
+     * <p>
+     * This method handles the history of all moves during the game. Passed
+     * FieldPoint point in conjunction with current ball position makes a move,
+     * that gets checked in move history.
+     *
+     * @param point of a move that should be checked in move history.
+     * @return true if specified move was already played, otherwise false.
+     */
+    private boolean isMoveInHistory(FieldPoint point) {
+        if (game.getMoveHistory().containsKey(point)) {
+            final List<FieldPoint> pointHist = game.getMoveHistory().get(point);
 
             if (!pointHist.isEmpty()) {
                 for (FieldPoint pt: pointHist) {
@@ -89,14 +140,31 @@ public class GameEvaluation {
         return false;
     }
 
-    private void checkBounce(FieldPoint dest) {
-        if (game.getMoveHistory().containsKey(dest) || isPointOnBounds(dest)) {
+    /**
+     * Method for checking bounce mechanic.
+     * <p>
+     * This method handles games bounce mechanic. If a bounce of a ball at the desired
+     * destination is detected, the same player is going to play. Otherwise, it's other
+     * players turn.
+     *
+     * @param point whose bounce should be checked.
+     */
+    private void checkBounce(FieldPoint point) {
+        if (game.getMoveHistory().containsKey(point) || isPointOnBounds(point)) {
             System.out.println("It's a bounce!!!");
         } else {
             turn = !turn;
         }
     }
 
+    /**
+     * Method for checking points location.
+     * <p>
+     * This method checks whether passed FieldPoint point is placed on bounds of the soccer field.
+     *
+     * @param point whose position should be checked.
+     * @return true if passed point is placed on bounds of a field, otherwise false.
+     */
     private boolean isPointOnBounds(FieldPoint point) {
         return ((point.row() == 0 || point.row() == game.getRows() - 1) ||
                 (point.column() == 0 || point.column() == game.getColumns() - 1)) &&
--- src/main/java/stc/soccer/GameSetup.java
+++ src/main/java/stc/soccer/GameSetup.java
@@ -8,6 +8,9 @@ public class GameSetup {
         throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Utility method to set up and start a new Code Soccer game.
+     */
     public static void setupGame() {
         Scanner in = new Scanner(System.in);
         int columns;
@@ -27,6 +30,15 @@ public class GameSetup {
         eval.play(in);
     }
 
+    /**
+     * Method for receiving data to set up a new game.
+     * <p>
+     * It is used for receiving number of columns and rows of the field. Both of
+     * these values have to be odd and bigger than five.
+     *
+     * @param in for using a pre-existing scanner.
+     * @return valid value received from the user.
+     */
     private static int getGameValues(Scanner in) {
         int value = in.nextInt();
 

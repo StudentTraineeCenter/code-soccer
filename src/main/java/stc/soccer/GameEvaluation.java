3dac2bf4755ec71ea5b0138f95e403f9d1b08625
Jakub Wodecki <wodecki@atlasgroup.cz>
2022-12-14 23:03:34 +0100

    Relocated methods associated with game mechanics

--- src/main/java/stc/soccer/GameEvaluation.java
+++ src/main/java/stc/soccer/GameEvaluation.java
@@ -1,16 +1,19 @@
 package stc.soccer;
 
-import java.util.List;
+import stc.soccer.opponents.ArtificialOpponent;
+
 import java.util.Scanner;
 
 public class GameEvaluation {
 
     private final SoccerGame game;
     private boolean turn;
+    private ArtificialOpponent ai;
 
-    public GameEvaluation(SoccerGame game) {
+    public GameEvaluation(SoccerGame game, ArtificialOpponent ai) {
         this.game = game;
         this.turn = true;
+        this.ai = ai;
     }
 
     /**
@@ -20,6 +23,7 @@ public class GameEvaluation {
      */
     public void play(Scanner in) {
         while (true) {
+            FieldPoint destination;
             turnOutput(turn);
 
             FieldPoint destination = new FieldPoint(in.nextInt(), in.nextInt());
@@ -29,11 +33,11 @@ public class GameEvaluation {
                 destination = new FieldPoint(in.nextInt(), in.nextInt());
             }
 
-            if (isPointInsideGoals(destination)) {
+            if (game.isPointInsideGoals(destination)) {
                 break;
             }
 
-            checkBounce(destination);
+            turn = changePlayers(turn, destination);
             game.addMove(destination);
         }
     }
@@ -50,124 +54,17 @@ public class GameEvaluation {
             System.out.println("Players " + game.getPlayer2() + " turn:");
         }
 
-        System.out.println("Current location of ball is " + game.getBallPosition().toString() + ".");
+        System.out.println("Current location of ball is " + game.getCurrentPosition().toString() + ".");
     }
 
-    /**
-     * Method for validation of a new move.
-     * <p>
-     * This method utilizes other methods to decide, whether a desired move is valid.
-     *
-     * @param dest whose generated move should be validated.
-     * @return true if desired move is valid, otherwise false.
-     */
-    private boolean isMoveValid(FieldPoint dest) {
-        return (isPointInsideBounds(dest) && isMoveMade(dest) &&
-                isPointCloseEnough(dest) && !isMoveInHistory(dest) &&
-                !(isPointOnBounds(game.getBallPosition()) && isPointOnBounds(dest))) ||
-                isPointInsideGoals(dest);
-    }
-
-    /**
-     * Method that checks whether a point is inside of bounds.
-     *
-     * @param point whose location should be checked.
-     * @return true if point is inside of bounds, otherwise false.
-     */
-    private boolean isPointInsideBounds(FieldPoint point) {
-        return ((point.row() >= 0) && (point.row() < game.getRows())) &&
-                ((point.column() >= 0) && (point.column() < game.getColumns()));
-    }
-
-    /**
-     * Method for checking whether a point is close enough to the ball.
-     *
-     * @param point whose location should be checked.
-     * @return true if point is close enough, otherwise false.
-     */
-    private boolean isPointCloseEnough(FieldPoint point) {
-        final FieldPoint position = game.getBallPosition();
-
-        return ((position.row() - point.row()) > -2 && (position.row() - point.row()) < 2) &&
-                ((position.column() - point.column()) > -2 && (position.column() - point.column()) < 2);
-    }
-
-    /**
-     * Method for checking whether a move was made.
-     *
-     * @param point whose relative position to the ball should be checked.
-     * @return true if a move was made, otherwise false.
-     */
-    private boolean isMoveMade(FieldPoint point) {
-        return (game.getBallPosition().row() != point.row() || game.getBallPosition().column() != point.column());
-    }
-
-    /**
-     * Method for checking goals.
-     *
-     * @param point whose location should be checked.
-     * @return true if point is located inside a goal, otherwise false.
-     */
-    private boolean isPointInsideGoals(FieldPoint point) {
-        return isPointCloseEnough(point) &&
-                (Math.abs(point.column() - game.getColumns() / 2) < 2) &&
-                (point.row() == -1 || point.row() == game.getRows());
-    }
-
-    /**
-     * Method for checking move history.
-     * <p>
-     * This method handles the history of all moves during the game. Passed
-     * FieldPoint point in conjunction with current ball position makes a move,
-     * that gets checked in move history.
-     *
-     * @param point of a move that should be checked in move history.
-     * @return true if specified move was already played, otherwise false.
-     */
-    private boolean isMoveInHistory(FieldPoint point) {
-        if (game.getMoveHistory().containsKey(point)) {
-            final List<FieldPoint> pointHist = game.getMoveHistory().get(point);
-
-            if (!pointHist.isEmpty()) {
-                for (FieldPoint pt: pointHist) {
-                    if (pt.equals(game.getBallPosition())) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Method for checking bounce mechanic.
-     * <p>
-     * This method handles games bounce mechanic. If a bounce of a ball at the desired
-     * destination is detected, the same player is going to play. Otherwise, it's other
-     * players turn.
-     *
-     * @param point whose bounce should be checked.
-     */
-    private void checkBounce(FieldPoint point) {
-        if (game.getMoveHistory().containsKey(point) || isPointOnBounds(point)) {
+    //TODO comment
+    private boolean changePlayers(boolean turn, FieldPoint point) {
+        if (game.checkBounce(point)) {
             System.out.println("It's a bounce!!!");
         } else {
             turn = !turn;
         }
-    }
 
-    /**
-     * Method for checking points location.
-     * <p>
-     * This method checks whether passed FieldPoint point is placed on bounds of the soccer field.
-     *
-     * @param point whose position should be checked.
-     * @return true if passed point is placed on bounds of a field, otherwise false.
-     */
-    private boolean isPointOnBounds(FieldPoint point) {
-        return ((point.row() == 0 || point.row() == game.getRows() - 1) ||
-                (point.column() == 0 || point.column() == game.getColumns() - 1)) &&
-                (point.column() != game.getColumns() / 2);
+        return turn;
     }
 }
--- src/main/java/stc/soccer/SoccerGame.java
+++ src/main/java/stc/soccer/SoccerGame.java
@@ -71,4 +71,137 @@ public class SoccerGame {
         }
     }
 
+    /**
+     * Method for validation of a new move.
+     * <p>
+     * This method utilizes other methods to decide, whether a desired move is valid.
+     *
+     * @param dest whose generated move should be validated.
+     * @return true if desired move is valid, otherwise false.
+     */
+    public boolean isMoveValid(FieldPoint dest) {
+        return (isPointInsideBounds(dest) && isMoveMade(dest) &&
+                isPointCloseEnough(dest) && !isMoveInHistory(dest) &&
+                !(isPointOnBounds(this.getCurrentPosition()) && isPointOnBounds(dest))) ||
+                isPointInsideGoals(dest);
+    }
+
+    /**
+     * Method that checks whether a point is inside of bounds.
+     *
+     * @param point whose location should be checked.
+     * @return true if point is inside of bounds, otherwise false.
+     */
+    public boolean isPointInsideBounds(FieldPoint point) {
+        return ((point.row() >= 0) && (point.row() < this.getRows())) &&
+                ((point.column() >= 0) && (point.column() < this.getColumns()));
+    }
+
+    /**
+     * Method for checking whether a point is close enough to the ball.
+     *
+     * @param point whose location should be checked.
+     * @return true if point is close enough, otherwise false.
+     */
+    public boolean isPointCloseEnough(FieldPoint point) {
+        final FieldPoint position = this.getCurrentPosition();
+
+        return ((position.row() - point.row()) > -2 && (position.row() - point.row()) < 2) &&
+                ((position.column() - point.column()) > -2 && (position.column() - point.column()) < 2);
+    }
+
+    /**
+     * Method for checking whether a move was made.
+     *
+     * @param point whose relative position to the ball should be checked.
+     * @return true if a move was made, otherwise false.
+     */
+    public boolean isMoveMade(FieldPoint point) {
+        return (this.getCurrentPosition().row() != point.row() || this.getCurrentPosition().column() != point.column());
+    }
+
+    /**
+     * Method for checking goals.
+     *
+     * @param point whose location should be checked.
+     * @return true if point is located inside a goal, otherwise false.
+     */
+    public boolean isPointInsideGoals(FieldPoint point) {
+        return isPointCloseEnough(point) &&
+                (Math.abs(point.column() - this.getColumns() / 2) < 2) &&
+                (point.row() == -1 || point.row() == this.getRows());
+    }
+
+    /**
+     * Method for checking move history.
+     * <p>
+     * This method handles the history of all moves during the this. Passed
+     * FieldPoint point in conjunction with current ball position makes a move,
+     * that gets checked in move history.
+     *
+     * @param point of a move that should be checked in move history.
+     * @return true if specified move was already played, otherwise false.
+     */
+    public boolean isMoveInHistory(FieldPoint point) {
+        if (this.getMoveHistory().containsKey(point)) {
+            final List<FieldPoint> pointHist = this.getMoveHistory().get(point);
+
+            if (!pointHist.isEmpty()) {
+                for (FieldPoint pt: pointHist) {
+                    if (pt.equals(this.getCurrentPosition())) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Method for checking bounce mechanic.
+     * <p>
+     * This method handles games bounce mechanic. If a bounce of a ball at the desired
+     * destination is detected, the same player is going to play. Otherwise, it's other
+     * players turn.
+     *
+     * @param point whose bounce should be checked.
+     * @return true if move does bounce, otherwise false
+     */
+    public boolean checkBounce(FieldPoint point) {
+        return this.getMoveHistory().containsKey(point) || isPointOnBounds(point);
+    }
+
+    /**
+     * Method for checking points location.
+     * <p>
+     * This method checks whether passed FieldPoint point is placed on bounds of the soccer field
+     * excluding goals.
+     *
+     * @param point whose position should be checked.
+     * @return true if passed point is placed on bounds of a field, otherwise false.
+     */
+    public boolean isPointOnBounds(FieldPoint point) {
+        return ((point.row() == 0 || point.row() == this.getRows() - 1) ||
+                (point.column() == 0 || point.column() == this.getColumns() - 1)) &&
+                (point.column() != this.getColumns() / 2);
+    }
+
+    /** TODO
+     * Method for checking possibility of a move from current position.
+     * <p>
+     * This method checks whether a move is possible from current ball position.
+     * A move is not possible if all other moves from the position were already made
+     * or are unable to be done due to the points placement on field bounds.
+     *
+     * @return true if any move is possible, otherwise false
+     */
+    public boolean checkMovePossibility() {
+        if (isPointOnBounds(this.getCurrentPosition())) {
+            return true;
+        } else {
+            return this.getMoveHistory().get(this.getCurrentPosition()).size() == 8;
+        }
+    }
+
 }
